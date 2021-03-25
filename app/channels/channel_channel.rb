e677d1ad556860a5ebb368bc70f8aea22f4a197f
NickyEXE <dovern42@gmail.com>
2021-03-24 22:03:54 -0400

    working commit

--- app/channels/channel_channel.rb
+++ app/channels/channel_channel.rb
@@ -1,8 +1,10 @@
 class ChannelChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "channel_channel_#{params[:id]}"
+    # stream_from "channel_channel"
     # stream_from "some_channel"
     @channel = Channel.find(params[:id])
+    puts "streaming for #{@channel.id}"
+    # puts "I am #{self}"
     stream_for @channel
   end
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,12 @@ class MessagesController < ApplicationController
     render json: @message
   end
 
+  def farts
+    @message = Channel.find(2).messages.create(content: "Farts", user: User.find(2))
+    puts @message.errors.full_messages.to_sentence
+    ChannelChannel.broadcast_to(@message.channel, @message)
+  end
+
   private
 
   def message_params
